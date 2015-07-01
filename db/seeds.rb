d161e86b11807c008b4a463e89caafa3cc73b763
amandeep singh <amandeep.bhamra@gmail.com>
2015-07-01 16:03:31 +0530

    seeds minor change

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-user = User.create(name: "Amandeep Singh", username: "amandeeps", email: "amandeep.bhamra@gmail.com", password: "testing", password_confirmation: "testing")
+@user = User.create(name: "Amandeep Singh", username: "amandeeps", email: "amandeep.bhamra@gmail.com", password: "testing", password_confirmation: "testing")
 
-100.times { Article.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraphs(10) , author_id: user.id) }
+100.times { Article.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraphs(10) , author_id: @user.id) }
